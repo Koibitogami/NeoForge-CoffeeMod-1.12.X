c171f2fce2ae0bdca370a3096fcdd0f30f49c066
Dank33 <mrrevo3036@gmail.com>
2025-04-08 18:48:18 -0400

    First two items prototypes in

--- src/main/java/com/Dank3/CoffeeMod/CoffeeMod.java
+++ src/main/java/com/Dank3/CoffeeMod/CoffeeMod.java
@@ -1,5 +1,6 @@
 package com.Dank3.CoffeeMod;
 
+import com.Dank3.CoffeeMod.block.ModBlocks;
 import com.Dank3.CoffeeMod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
@@ -42,6 +43,7 @@ public class CoffeeMod
         NeoForge.EVENT_BUS.register(this);
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -61,6 +63,11 @@ public class CoffeeMod
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
           event.accept(ModItems.BISMUTH);
           event.accept((ModItems.RAW_BISMUTH));
+        }
+
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS){
+            event.accept(ModBlocks.BISMUTH_BLOCK);
+            event.accept(ModBlocks.BISMUTH_ORE);
         }
     }
 
---
+++ src/main/java/com/Dank3/CoffeeMod/block/ModBlocks.java
@@ -0,0 +1,50 @@
+package com.Dank3.CoffeeMod.block;
+
+import com.Dank3.CoffeeMod.item.ModItems;
+import net.minecraft.client.resources.sounds.Sound;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import com.Dank3.CoffeeMod.CoffeeMod;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+
+    public static final DeferredRegister.Blocks BLOCKS =
+            DeferredRegister.createBlocks(CoffeeMod.MOD_ID);
+
+    // creates a block with a name, the () is the registry and -> new Block(...) where the "..." are the properties of the block
+    // .of() is more like an adjective
+    public static final DeferredBlock<Block> BISMUTH_BLOCK = registerBlock("bismuth_block",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .strength(4f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST)));
+
+    public static final DeferredBlock<Block> BISMUTH_ORE = registerBlock("bismuth_ore",
+            () -> new DropExperienceBlock(UniformInt.of(2, 4),
+                    BlockBehaviour.Properties.of().strength(3f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
+
+    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block){
+        DeferredBlock<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+
+    private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block){
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus){
+        BLOCKS.register(eventBus);
+    }
+}
+
+
